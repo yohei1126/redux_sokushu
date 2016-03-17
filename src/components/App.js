9098c0fb76e564201aaf9027c3ab4dc5c46b7565
Mamoru Amano <booktower103@gmail.com>
2016-03-17 11:52:13 +0900

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddTodo from './AddTodo'
+import VisibleTodoList from './VisibleTodoList'
+
+const App = () => (
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+  </div>
+)
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: './dist',
+    filename: "bundle.js",
+    publicPath: '/'
+  },
+  devServer: {
+    inline: true,
+    contentBase: './dist',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
