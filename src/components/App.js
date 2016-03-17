90a5969b5761bc4d249525c084fcfa6e80e7369e
Yohei Onishi <vivre214@gmail.com>
2016-03-17 20:49:25 +0900

    init

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import AddTodo from '../components/AddTodo'
+import VisibleTodoList from '../components/TodoList'
+
+const App = () => (
+  <div>
+    <AddTodo />
+    <TodoList />
+  </div>
+)
+export default App 
---
+++ src/components/TodoList.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { toggleTodo } from '../actions'
+
+const Todo = ({ onClick, completed, text }) => (
+  <li
+    onClick={onClick}
+    style={{
+      textDecoration: completed ? 'line-through' : 'none'
+    }}
+  >
+   {text}
+  </li>
+)
+
+const Todos = ({ todos, onTodoClick }) => (
+  <ul>
+    {todos.map(todo =>
+      <Todo
+        key={todo.id}
+        {...todo}
+        onClick={() => onTodoClick(todo.id)}
+      />
+    )}
+  </ul>
+)
+
+const mapStateToProps = (state) => {
+  return {
+    todos: state.todos
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTodoClick: (id) => {
+      dispatch(toggleTodo(id))
+    }
+  }
+}
+
+const TodoList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Todos)
+
+export default TodoList 
